8e507ac7eda194b49ad11c4c695de50fd7579c4d
Cynthia R <Crittenbach@gmail.com>
2026-07-18 18:43:40 -0400

    Create exercise5.c

---
+++ chapter4/exercise5.c
@@ -0,0 +1,251 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+
+
+#define MAXOP 100
+#define NUMBER '0'
+
+#define MAXVAL 100
+#define BUFSIZE 100
+
+int getop(char s[]);
+void push(double f);
+double pop(void);
+int getch(void);
+void ungetch(int c);
+double peek(double s[]);
+void swap(void); 
+void clearStack(void);
+
+int sp = 0;
+double val[MAXVAL];
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+/* Reverse Polish calculator */
+int main(void)
+{
+    int type;
+    double op2;
+    char s[MAXOP];
+    
+    printf("%s\n", "type ? to print top element of stack");
+    printf("%s\n", "type D to duplicate top element of stack");
+    printf("%s\n", "type ~ to swap the top 2 elements of the stack");
+    printf("%s\n", "type C to clear the stack");
+    printf("%s\n", "type s for sin");
+    printf("%s\n", "type e for exp");
+    printf("%s\n", "type p for pow");
+
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+        case NUMBER:
+            push(atof(s));
+            break;
+
+        case '+':
+            push(pop() + pop());
+            break;
+
+        case '*':
+            push(pop() * pop());
+            break;
+
+        case '-':
+            op2 = pop();
+            push(pop() - op2);
+            break;
+
+        case '/':
+            op2 = pop();
+
+            if (op2 != 0.0)
+                push(pop() / op2);
+            else
+                printf("error: zero divisor\n");
+
+            break;
+        
+        case '%':
+            op2 = pop();
+            // % for ints
+            /*
+            if (op2 != 0.0) {
+                push((int)pop() % (int)op2);
+            } else {
+                printf("error: zero modulo divisor\n");
+            }
+            */
+            //% for floating point
+            if (op2 != 0.0) {
+                push(fmod(pop(), op2));
+            } else {
+                printf("error: zero modulo divisor\n");
+            }
+            
+            break;
+            
+        case '?':
+            printf("%g\n", peek(val));
+            break;
+            
+        case  'D':
+            if (sp > 0)
+                push(peek(val));
+            else
+                printf("error: stack empty\n");
+            break;
+            
+        case '~': 
+            if (sp > 1) {
+                swap();
+            }
+            break;
+        
+        case 'C':
+            clearStack();
+            break;
+            
+        case 's':
+            if (sp >= 1)
+                push(sin(pop()));
+            else
+                printf("error: sin requires one operand\n");
+            break;
+        
+        case 'e':
+            if (sp >= 1)
+                push(exp(pop()));
+            else
+                printf("error: exp requires one operand\n");
+            break;
+        
+        case 'p':
+            if (sp >= 2) {
+                op2 = pop();         
+                push(pow(pop(), op2)); 
+            } else {
+                printf("error: pow requires two operands\n");
+            }
+            break;
+
+        case '\n':
+        if (sp > 0) {
+             printf("\t%.8g\n", pop());
+        }
+        break;
+
+        default:
+            printf("error: unknown command %s\n", s);
+            break;
+        }
+    }
+
+    return 0;
+}
+
+void push(double f)
+{
+    if (sp < MAXVAL) {
+        val[sp++] = f;
+    } else {
+        printf("error: stack full, can't push %g\n", f);
+    }
+}
+
+double pop(void)
+{
+    if (sp > 0) {
+        return val[--sp];
+    }
+
+    printf("error: stack empty\n");
+    return 0.0;
+}
+
+int getop(char s[])
+{
+    int i = 0;
+    int c;
+    int next;
+
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+
+    s[1] = '\0';
+
+    if (c == '-') {
+        next = getch();
+
+        if (!isdigit(next) && next != '.') {
+            ungetch(next);
+            return '-';
+        }
+
+        s[0] = '-';
+        s[1] = c = next;
+        i = 1;
+    } else if (!isdigit(c) && c != '.') {
+        return c;
+    }
+
+    /* Collect integer part. */
+    if (isdigit(c)) {
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    }
+
+    /* Collect fractional part. */
+    if (c == '.') {
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    }
+
+    s[i] = '\0';
+
+    if (c != EOF)
+        ungetch(c);
+
+    return NUMBER;
+}
+
+
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+    if (bufp >= BUFSIZE) {
+        printf("ungetch: too many characters\n");
+    } else {
+        buf[bufp++] = c;
+    }
+}
+
+double peek(double stack[])
+{
+    if (sp > 0) {
+        return stack[sp - 1];
+    }
+
+    printf("error: stack empty\n");
+    return 0.0;
+}
+
+void swap(void) { 
+    double copy1 = val[sp - 1];
+    double copy2 = val[sp - 2];
+    val[sp - 1] = copy2;
+    val[sp - 2] = copy1;
+    
+    return;
+}
+
+void clearStack(void) {
+   sp = 0;
+}
